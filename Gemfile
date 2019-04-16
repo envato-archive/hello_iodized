94ceb7890e8154c6e06c52693c3acdd7c4cb7ab0
Peter Sumskas <peter.sumskas@envato.com>
2019-04-16 16:32:56 +1000

    framework to drive websocket work

--- Gemfile
+++ Gemfile
@@ -3,4 +3,5 @@
 source "https://rubygems.org"
 
 gem "sinatra"
-gem "iodized2_ruby_client", github: "envato/iodized2_ruby_client", branch: "master"
+# gem "iodized2_ruby_client", github: "envato/iodized2_ruby_client", branch: "master"
+gem "iodized2_ruby_client", path: "../iodized2_ruby_client"
--- hello.rb
+++ hello.rb
@@ -1,5 +1,12 @@
 require "sinatra"
+require "iodized2_ruby_client"
+
+iodized = Iodized2RubyClient.new("ws://localhost:4000/features_socket/websocket")
 
 get "/" do
   erb :hello, locals: {awesome_feature: false}
+end
+
+get "/features" do
+  erb :features, locals: { features: iodized.features}
 end
