46cae67d4f40c313f91e744a2455d30f2aff4855
Peter Sumskas <peter.sumskas@envato.com>
2019-04-17 22:07:36 +1000

    support env vars for configuration

--- hello.rb
+++ hello.rb
@@ -1,7 +1,10 @@
 require "sinatra"
 require "iodized2_ruby_client"
 
-iodized = Iodized2RubyClient.new("ws://localhost:4000/features_socket/websocket", "key", "secret")
+iodized_url = ENV.fetch("IODIZED_URL", "ws://localhost:4000/features_socket/websocket")
+iodized_key = ENV.fetch("IODIZED_KEY", "key")
+iodized_secret = ENV.fetch("IODIZED_SECRET", "secret")
+iodized = Iodized2RubyClient.new(iodized_url, iodized_key, iodized_secret)
 
 get "/" do
   erb :hello, locals: {awesome_feature: iodized.enabled?('awesome_feature') }
